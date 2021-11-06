8ee4f402f8f4be89585176c6f00b0e0ce6ed696b
Anh Van <ngockhueanhvan@gmail.com>
2021-11-06 19:57:09 +1100

    ignore credentials

--- code/pushing_data_bq.py
+++ code/pushing_data_bq.py
@@ -10,13 +10,14 @@ df = data_cleaning.df
 
 # 2. Downloading the data
 
-filename =  os.path.join(fileDirectory, 'may-eleventh-e207a3fd73f9.json')  #BQ access key
-bq_client = bigquery.Client.from_service_account_json(filename) 
+filename = os.path.join(
+    fileDirectory, 'may-eleventh-e207a3fd73f9.json')  # BQ access key
+bq_client = bigquery.Client.from_service_account_json(filename)
 table_config = bigquery.job.LoadJobConfig(write_disposition='WRITE_TRUNCATE')
 table_config.autodetect = True
 
 
 # 3. Pushing the data back to BQ
 job = bq_client.load_table_from_dataframe(
-        df,'may-eleventh.food_supply_reporting.fao_food_supply',job_config=table_config)
+    df, 'may-eleventh.food_supply_reporting.fao_food_supply', job_config=table_config)
 job.result()  # Waits for table load to complete.
